645d10d6203d76b518856d96fa4f344daab929d9
Ahmed <ahmedali.96@hotmail.com>
2020-06-30 15:15:53 -0400

    oAuth login setup

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { GoogleLogin } from "react-google-login";
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
 
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+  const onSuccess = (googleUser) => {
+    console.log({ googleUser });
+  };
+  return (
+    <GoogleLogin
+      clientId="784472468796-igkb3ds44vjvcon4o1dusjivqtn6qoeu.apps.googleusercontent.com"
+      onSuccess={onSuccess}
+      onFailure={(err) => console.log("fail", err)}
+    />
+  );
 };
 
 const styles = {
@@ -12,8 +22,8 @@ const styles = {
     display: "flex",
     justifyContent: "center",
     flexDirection: "column",
-    alignItems: "center"
-  }
+    alignItems: "center",
+  },
 };
 
 export default withStyles(styles)(Login);
