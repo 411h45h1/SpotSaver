ffc1b3956cec66dbae0b72c16b8a305e5dc13152
Ahmed <ahmedali.96@hotmail.com>
2020-06-30 13:43:53 -0400

    mongoDB connected

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
+//graphql
 const { ApolloServer } = require("apollo-server");
-
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
 
+//MongoDB
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.Mongo_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected!"))
+  .catch((err) => console.error(err));
+
+// Apollo Server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
